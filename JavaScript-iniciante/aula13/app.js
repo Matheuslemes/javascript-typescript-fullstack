6fdfc0ad04978b065c2268ee59a212799fb6577c
Matheuslemes <matheuslemesmsl@gmail.com>
2024-07-17 16:50:39 -0300

    Aula 13 com exercicios

---
+++ JavaScript-iniciante/aula13/app.js
@@ -0,0 +1,11 @@
+// Objeto Math
+let num01 = 9.54578;
+let num02 = Math.floor(num01); // arredonda o numero para baixo
+let num03 = Math.ceil(num01) // arredonda o numero para cima
+let num04 = Math.round(); // arredonda para o numero mais proximo
+
+console.log(Math.max(1, 2, 3, 49, 800, -50)); // encontra o maior número da sequencia
+console.log(Math.min(1, 2, 3, 49, 800, -50)); // encontra o menor número da sequencia
+console.log(Math.random()); // gera um número aleatório sempre que é chamado - 
+// math.pi -> valor de pi, math.pow -> potenciação
+console.log(num01 ** 0.5); // pega a raiz quadrada do número
