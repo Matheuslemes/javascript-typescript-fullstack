f9b3576173da567897886bb1ec70139b420d85e0
Matheuslemes <matheuslemesmsl@gmail.com>
2024-07-31 16:53:31 -0300

    aulas 09 e 10

---
+++ JavaScript-lógica/aula10/app.js
@@ -0,0 +1,9 @@
+// Diferenças entre var e let/const
+
+// let tem escopo de bloco
+// var tem escopo de função
+
+const verdadeira = true;
+
+let nome1 = 'lemes'; // não é possivel redeclarar
+var nome2 = 'silva'; // é possivel redeclarar
